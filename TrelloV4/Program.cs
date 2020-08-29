8d19da97519807078b9c3a64753123ce0ab59a8e
tjgtll <urkona@list,ru>
2020-08-29 15:27:42 +0300

    hw13

--- TrelloV4/Program.cs
+++ TrelloV4/Program.cs
@@ -60,14 +60,26 @@ namespace TrelloV4
         {
             UserManager userManager = UserManager.Instance;
             userManager = JsonConvert.DeserializeObject<UserManager>(File.ReadAllText("users.json"));
-            Console.WriteLine(userManager.Add("Володин", "Николай","Юрьевич", "urkona@list.ru", "1111"));
+            userManager.Add("Володин", "Николай","Юрьевич", "urkona@list.ru", "1111");
             File.WriteAllText("users.json", JsonConvert.SerializeObject(userManager));
-            //ну например есть пользователь, при первом заходе сообщений 0
-            Console.WriteLine("Первый проход");
+            
             try
             {
                 userManager = JsonConvert.DeserializeObject<UserManager>(File.ReadAllText("users.json"));
-                ClientManager client = new ClientManager(userManager.Entry("urkona@list.ru", "1111"));
+                //Если пароль или логин неверный то в e.Message будет сообщение об этом 
+                ClientManager client = new ClientManager(userManager.Entry("urkona@list.ru", "1121"));
+                File.WriteAllText("users.json", JsonConvert.SerializeObject(userManager));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            //неверный логин
+            try
+            {
+                userManager = JsonConvert.DeserializeObject<UserManager>(File.ReadAllText("users.json"));
+                //Если пароль или логин неверный то в e.Message будет сообщение об этом 
+                ClientManager client = new ClientManager(userManager.Entry("urkona@list", "1111"));
                 File.WriteAllText("users.json", JsonConvert.SerializeObject(userManager));
             }
             catch (Exception e)
@@ -75,24 +87,6 @@ namespace TrelloV4
                 Console.WriteLine(e.Message);
             }
 
-
-
-            BoardManager boardManager = BoardManager.Instance;
-            boardManager.Add(1, "доска 1");
-            boardManager.List[0].Add(new Task("задача 1", "Тект задача 1", 1));
-            boardManager.List[0].tasks[0].Take(1);
-            boardManager.List[0].tasks[0].Check(1);
-            Console.WriteLine("Второй проход");
-            try
-            {
-                userManager = JsonConvert.DeserializeObject<UserManager>(File.ReadAllText("users.json"));
-                ClientManager client = new ClientManager(userManager.Entry("urkona@list.ru", "1111"));
-                File.WriteAllText("users.json", JsonConvert.SerializeObject(userManager));
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
         }
     }
 }
